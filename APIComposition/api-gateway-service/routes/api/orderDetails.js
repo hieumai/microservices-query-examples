26e6a08c318e4160fd61e970541a0a3a1a785779
Hieu Mai <hieu.mai@hotschedules.com>
2019-02-22 14:19:41 +0700

    Update api-gateway to handle services' errors

--- APIComposition/api-gateway-service/routes/api/orderDetails.js
+++ APIComposition/api-gateway-service/routes/api/orderDetails.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import rp from 'request-promise-native';
-import { from, forkJoin } from 'rxjs';
-import { reduce, map } from 'rxjs/operators';
+import { from, forkJoin, of } from 'rxjs';
+import { reduce, map, catchError } from 'rxjs/operators';
 import { SERVICES_CONNECT_OPTIONS } from '../../constants';
 
 const router = express.Router();
@@ -14,33 +14,46 @@ router.get('/:id', async (req, res, next) => {
     const ticketObservable = from(rp(SERVICES_CONNECT_OPTIONS.kitchenOptions(`tickets?orderId=${req.params.id}`)))
       .pipe(map(tickets => {
         const result = (tickets && tickets.length > 0) ? tickets[0] : null;
-        return { ticket: result }
+        return { ticket: result };
+      }), catchError((err) => {
+        console.log('ticket error', err.message);
+        return of({ ticket: null });
       }));
     const billObservable = from(rp(SERVICES_CONNECT_OPTIONS.accountingOptions(`bills?orderId=${req.params.id}`)))
       .pipe(map(bills => {
         const result = (bills && bills.length > 0) ? bills[0] : null;
-        return { bill: result }
+        return { bill: result };
+      }), catchError((err) => {
+        console.log('bill error', err.message);
+        return of({ bill: null });
       }));
 
     const deliveryObservable = from(rp(SERVICES_CONNECT_OPTIONS.delivery(`deliveries?orderId=${req.params.id}`)))
       .pipe(map(deliveries => {
         const result = (deliveries && deliveries.length > 0) ? deliveries[0] : null;
-        return { delivery: result }
+        return { delivery: result };
+      }), catchError((err) => {
+        console.log('delivery error', err.message);
+        return of({ delivery: null });
       }));
 
     const orderObservable = from(rp(SERVICES_CONNECT_OPTIONS.order(`orders/${req.params.id}`)))
-      .pipe(map(order => { return { order } }));
+      .pipe(map(order => { return { order }; }),
+        catchError((err) => {
+          console.log('order error', err.message);
+          return of({ order: null });
+        }));
 
     forkJoin(ticketObservable, billObservable, deliveryObservable, orderObservable)
       .subscribe(val => {
-      const result = val.reduce((acc, item) => {
-        return {
-          ...acc,
-          ...item
-        }
+        const result = val.reduce((acc, item) => {
+          return {
+            ...acc,
+            ...item
+          };
+        });
+        res.json(result);
       });
-      res.json(result);
-    });
   } catch (error) {
     next(error);
   }
